9204396028c5a358750db07c7724502ef7010a72
ABDERRAHIM AIT TATA <aittataa@gmail.com>
2021-10-02 17:17:51 +0100

    commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,23 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
 import 'package:get/get.dart';
-import 'package:i_store/constant/constant.dart';
-import 'package:i_store/constant/messages.dart';
-import 'package:i_store/widgets/splash.dart';
 
 void main() {
-  WidgetsFlutterBinding.ensureInitialized();
-  SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp]);
-  SystemChrome.setSystemUIOverlayStyle(
-    SystemUiOverlayStyle(
-      systemNavigationBarColor: Colors.black54,
-      systemNavigationBarDividerColor: Colors.transparent,
-      systemNavigationBarIconBrightness: Brightness.light,
-      statusBarColor: Colors.transparent,
-      statusBarBrightness: Brightness.dark,
-      statusBarIconBrightness: Brightness.dark,
-    ),
-  );
   runApp(MyApp());
 }
 
@@ -26,13 +10,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return GetMaterialApp(
       debugShowCheckedModeBanner: false,
-      title: Messages.APP_TITLE,
-      color: mainColor,
-      theme: ThemeData(
-        primaryColor: mainColor,
-        scaffoldBackgroundColor: mainColor,
-      ),
-      home: Splash(),
+      // title: Messages.APP_TITLE,
     );
   }
 }
--- lib/src/core/utils/constants.dart
+++ lib/src/core/utils/constants.dart
@@ -25,17 +25,3 @@ class Constant {
     );
   }
 }
-
-Color mainColor = Color(0xFFF0F0F0);
-
-//get getRandomColor => Color(Random().nextInt(0xFFFFFFFF)).withAlpha(0xFF).value;
-
-// List<Categories> categoriesList = [
-//   Categories(id: 1, title: "All"),
-//   Categories(id: 1, title: "Phone"),
-//   Categories(id: 1, title: "Laptop"),
-//   Categories(id: 1, title: "Desktop"),
-//   Categories(id: 1, title: "Battery"),
-//   Categories(id: 1, title: "Cables"),
-//   Categories(id: 1, title: "Accessories"),
-// ];
