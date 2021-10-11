21c6fcca40f7d79b74330f29df8716cb02b60ed7
ABDERRAHIM AIT TATA <aittataa@gmail.com>
2021-10-11 19:50:59 +0100

    commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,23 +1,40 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:get/get.dart';
-
-import 'app/routes/app_pages.dart';
+import 'package:i_store/app/config/messages/app_message.dart';
+import 'package:i_store/app/config/themes/app_theme.dart';
+import 'package:i_store/app/routes/app_pages.dart';
 
 void main() {
+  configureDependencies();
+  WidgetsFlutterBinding.ensureInitialized();
+  SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp]);
+  SystemChrome.setSystemUIOverlayStyle(
+    const SystemUiOverlayStyle(
+      systemNavigationBarColor: AppTheme.whiteBackColor,
+      systemNavigationBarIconBrightness: Brightness.dark,
+      statusBarColor: AppTheme.transparentColor,
+      statusBarIconBrightness: Brightness.dark,
+    ),
+  );
   runApp(iStore());
 }
 
 class iStore extends StatelessWidget {
   const iStore({
-    Key? key
+    Key? key,
   }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return GetMaterialApp(
-      title: "Application",
+      debugShowCheckedModeBanner: false,
+      title: AppMessage.appTitle,
+      theme: AppTheme.light,
       initialRoute: AppPages.INITIAL,
       getPages: AppPages.routes,
     );
   }
 }
+
+configureDependencies(){}
